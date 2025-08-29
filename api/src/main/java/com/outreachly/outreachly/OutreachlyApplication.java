347d9f64581672188fc2857b4c81986f30936926
Viet My <mynv2712@gmail.com>
2025-08-29 13:15:41 -0400

    Initial commit: Add Spring Boot project structure and .gitignore

---
+++ api/src/main/java/com/outreachly/outreachly/OutreachlyApplication.java
@@ -0,0 +1,13 @@
+package com.outreachly.outreachly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutreachlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutreachlyApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/outreachly/outreachly/OutreachlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.outreachly.outreachly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutreachlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
